ce1e6ffc432dcd9c19c16daf32967e2e424d6bb2
iwater <iwater@gmail.com>
2019-11-30 12:26:09 +0000

    Deploying to gh-pages from master a699017cf759ac5c990aebba8729c2eb6a8d2006

---
+++ precache-manifest.e752633595956a2d8bf28f841b4acc23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb42a09da77632da0041045c0609b7f",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "b6fcc07fb20781cb3069",
+    "url": "/github-actions-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "555149349d442c73b1c4",
+    "url": "/github-actions-demo/static/js/2.dbe21ac2.chunk.js"
+  },
+  {
+    "revision": "b6fcc07fb20781cb3069",
+    "url": "/github-actions-demo/static/js/main.210d7d95.chunk.js"
+  },
+  {
+    "revision": "c448e9729eca4ab15cc9",
+    "url": "/github-actions-demo/static/js/runtime-main.bab7c5fc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/github-actions-demo/static/media/logo.25bf045c.svg"
+  }
+]);
